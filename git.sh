f1c35d8f51f92bb4a29e2e9c1c4c753058d32f77
Phil Watson <phil@rakhama.com>
2024-10-28 10:52:44 +1000

    Added Git config setup

---
+++ git.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+read -p "Your full name? " username
+
+read -p "Your email? " email
+
+read -p "Default Editor? (default: nano) " editor
+editor="${name:=nano}"
+
+git config --global user.name "$username"
+git config --global user.email "$email"
+git config --global push.default "current"
+git config --global pull.rebase "false"
+git config --global core.editor "$editor"
+
+echo "All done!"
+
+git config list --global | cat
