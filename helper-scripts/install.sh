9e6b23fcaae1b35ba3162181bb26d238194efbf5
Phil <phil@rakhama.com>
2025-03-31 20:34:59 +1000

    But we still need sudo

--- helper-scripts/install.sh
+++ helper-scripts/install.sh
@@ -6,18 +6,18 @@ echo Installing the following apps: $@
 
 if [ -x "$(command -v apk)" ];
 then
-    apk add --no-cache -y $@
+    sudo apk add --no-cache -y $@
 elif [ -x "$(command -v apt-get)" ];
 then
-    apt-get update
-    apt-get install -y $@
+    sudo apt-get update
+    sudo apt-get install -y $@
 elif [ -x "$(command -v dnf)" ];
 then
-    dnf check-update
-    dnf install -y $@
+    sudo dnf check-update
+    sudo dnf install -y $@
 elif [ -x "$(command -v zypper)" ];
 then
-    zypper install -y $@
+    sudo zypper install -y $@
 else
     echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $@">&2;
 fi
